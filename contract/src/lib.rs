3c4510ba0e31d647b6aa1b81cc1cec312f06a011
sopheaksem1993 <sopheaksem1993@gmail.com>
2021-05-12 03:10:18 +0800

    Set the sender address, read the address of sender

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,17 +1,23 @@
 // Contract doesn't use Rust's standard library
 #![no_std]
 
-// `pwasm-ethereum` implements bindings to the runtime
 extern crate pwasm_ethereum;
+extern crate pwasm_std;
 
-/// Will be described in the next step
+use pwasm_std::types::H256;
+
+/// Deploy will be executed only once on deployment, but will not be stored on blockchain node
 #[no_mangle]
 pub fn deploy() {
+    // Set the sender address to the contract storage at address zero
+    pwasm_ethereum::write(&H256::zero().into(), &H256::from(pwasm_ethereum::sender()).into());
 }
 
 /// The call function is the main function of the *deployed* contract
 #[no_mangle]
 pub fn call() {
+    // Read the address of the deployer
+    let owner = pwasm_ethereum::read(&H256::zero().into());
     // Send a result pointer to the runtime
-    pwasm_ethereum::ret(&b"result"[..]);
+    pwasm_ethereum::ret(owner.as_ref());
 }
